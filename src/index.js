7ee7d5a5f1d9e44c3ee562cd4b4e85df7406190d
yossydev <codeyy021@gmail.com>
2024-06-08 00:44:04 +0900

    chore: babel-loaderを使うようにしてアップデート

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Foo } from "./foo";
 
 function Counter() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,8 @@ function Counter() {
     <>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+
+      <Foo />
     </>
   );
 }
